1e1509cffe1ab8d05323ea43f0a8e1527a2f294b
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-05-16 11:36:58 -0300

    ProductDTO with categories

--- src/main/java/com/muccio/dscommerce/services/ProductService.java
+++ src/main/java/com/muccio/dscommerce/services/ProductService.java
@@ -1,7 +1,9 @@
 package com.muccio.dscommerce.services;
 
+import com.muccio.dscommerce.dto.CategoryDTO;
 import com.muccio.dscommerce.dto.ProductDTO;
 import com.muccio.dscommerce.dto.ProductMinDTO;
+import com.muccio.dscommerce.entities.Category;
 import com.muccio.dscommerce.entities.Product;
 import com.muccio.dscommerce.repositories.ProductRepository;
 import com.muccio.dscommerce.services.exceptions.DatabaseException;
@@ -72,6 +74,12 @@ public class ProductService {
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
+        entity.getCategories().clear();
+        for (CategoryDTO catDto : dto.getCategories()) {
+            Category cat = new Category();
+            cat.setId(catDto.getId());
+            entity.getCategories().add(cat);
+        }
     }
 
 
