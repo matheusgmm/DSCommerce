a0dd914ac7ffb5eb73993b42104c1fa4219bb441
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-05-21 11:51:17 -0300

    GET categories

---
+++ src/main/java/com/muccio/dscommerce/services/CategoryService.java
@@ -0,0 +1,23 @@
+package com.muccio.dscommerce.services;
+
+import com.muccio.dscommerce.dto.CategoryDTO;
+import com.muccio.dscommerce.entities.Category;
+import com.muccio.dscommerce.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> result = categoryRepository.findAll();
+        return result.stream().map(CategoryDTO::new).toList();
+    }
+}
